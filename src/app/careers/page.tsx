6e30ae3d65e1234b1a8e452ee766331c922a56b0
Pedrael <mihail.krivic@gmail.com>
2025-11-14 15:59:17 +0200

    refactor Opportunities component: add isEmpty prop for conditional rendering of job listings, and clean up formatting in careers page

--- src/app/careers/page.tsx
+++ src/app/careers/page.tsx
@@ -1,8 +1,8 @@
-import {Opportunities, Placeholder, Values} from "@/components/blocks";
+import { Opportunities, Placeholder, Values } from "@/components/blocks";
 import { Breadcrumbs } from "@/components";
 import React from "react";
 
-export default function Page () {
+export default function Page() {
   return (
     <div>
       <div className="container bg-background px-4 md:px-8 pb-c-50 md:pb-c-60 lg:px-c-50 lg:pb-20 xl:px-c-200 xl:pb-c-100">
@@ -13,13 +13,10 @@ export default function Page () {
         <h1 className="text-1xl mb-8">
           You’re free: Freedom of time, location, and to balance work and life.
         </h1>
-
       </div>
-      <Placeholder size={'tall'}>
-        Gallery
-      </Placeholder>
-      <Opportunities />
+      <Placeholder size={"tall"}>Gallery</Placeholder>
+      <Opportunities isEmpty={true} />
       <Values isDark={false} />
     </div>
-  )
+  );
 }
