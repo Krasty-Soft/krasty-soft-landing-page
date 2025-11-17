0b73f7b42e1b9e455d74a889626701d9ca6e8e59
Pedrael <mihail.krivic@gmail.com>
2025-11-17 22:41:45 +0200

    comment out Breadcrumbs component in careers page for future use and update Opportunities component visibility comment

--- src/app/careers/page.tsx
+++ src/app/careers/page.tsx
@@ -1,11 +1,10 @@
 import { Opportunities, Values } from "@/components/blocks";
-import { Breadcrumbs } from "@/components";
 import React from "react";
 
 export default function Page() {
   return (
     <div>
-      <div className="container bg-background px-4 md:px-8 pb-c-50 md:pb-c-60 lg:px-c-50 lg:pb-20 xl:px-c-200 xl:pb-c-100">
+      {/* <div className="container bg-background px-4 md:px-8 pb-c-50 md:pb-c-60 lg:px-c-50 lg:pb-20 xl:px-c-200 xl:pb-c-100">
         <div className="pt-6 pb-8">
           <Breadcrumbs />
         </div>
@@ -13,7 +12,7 @@ export default function Page() {
         <h1 className="text-1xl mb-8">
           You’re free: Freedom of time, location, and to balance work and life.
         </h1>
-      </div>
+      </div> */}
       {/* set isEmpty={false} to show the jobs */}
       <Opportunities isEmpty={true} />
       <Values isDark={false} />
